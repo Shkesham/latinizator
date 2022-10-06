17ce6a878e3cc0b4bd357052024685e39bd9ef7e
FelixTisov <fedya002.ks@gmail.com>
2022-10-06 11:39:10 +0300

    header added

---
+++ src/components/header/header.js
@@ -0,0 +1,31 @@
+import './header.css'
+
+function Header() {
+    return(
+        <header>
+                <div className='half-block' id='half-left'>
+                    <div className='logo-cont'>
+                        <div className='logo-image'/>
+                        <div className='title-cont'>
+                            <p>Шкешам Латинизатор</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='half-block' id='half-right'>
+                    <nav>
+                        <div className='nav-item'><p>Наши преокты</p></div>
+                        <div className='nav-item'><p>О Шкешам</p></div>
+                        <div className='nav-item'>
+                            <select>
+                                <option>Русский</option>
+                                <option>Олык Мари</option>
+                            </select>
+                        </div>
+                    </nav>
+                </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import Header from '../components/header/header'
+import '../styles/main.css'
+
+function Main() {
+    return(
+        <div className='wrapper'>
+            <Header/>
+        </div>
+    )
+}
+
+export default Main
