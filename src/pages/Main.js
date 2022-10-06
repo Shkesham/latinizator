eedadf5b2aa17c7d4a2dcc0e09b358be8b6d5b09
FelixTisov <fedya002.ks@gmail.com>
2022-10-06 12:35:18 +0300

    layout added

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,7 +4,30 @@ import '../styles/main.css'
 function Main() {
     return(
         <div className='wrapper'>
+
             <Header/>
+
+            <div className='main-cont'>
+
+                <div className='select-cont'>
+                    <div className='language-option'><p id='left-option'>Кириллица</p></div>
+                    <div className='change-options'></div>
+                    <div className='language-option'><p id='right-option'>Латиница</p></div>
+                </div>
+
+                <div className='text-block-cont'>
+                    <div className='text-block' id='text-block-left'>
+                        <textarea></textarea>
+                    </div>
+                    <div className='text-block' id='text-block-right'>
+                        <textarea readOnly={true}></textarea>
+                    </div>
+                </div>
+                
+
+
+            </div>
+
         </div>
     )
 }
